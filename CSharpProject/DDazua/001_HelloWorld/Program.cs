66d3f0abc6263d9aff4f4d73b4df6057b50273e5
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-10 02:22:17 +0900

    2024-08-10 C# 1-3 Study

---
+++ CSharpProject/DDazua/001_HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace _001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");                                           
+        }
+    }
+}
