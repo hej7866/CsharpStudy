a3dd25d6a2288b0216ffb36e819445c18c69d5bd
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-11 00:15:35 +0900

    2024-08-11 C# 1-4 Study

--- CSharpProject/DDazua/001_HelloWorld/Program.cs
+++ CSharpProject/DDazua/001_HelloWorld/Program.cs
@@ -5,9 +5,19 @@ namespace _001_HelloWorld
 {
     class Program
     {
+        //static void Main()
+        //{
+        //    Console.WriteLine("Hello World");
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");                                           
+            Console.WriteLine("Hello World!");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("arg: " + args[0]);
+            }
         }
     }
 }
---
+++ CSharpProject/DDazua/001_Mission/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Mission
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    switch (i)
+            //    {
+            //        case 0:
+            //        case 4:
+            //            Console.WriteLine("  *  ");
+            //            break;
+            //        case 1:
+            //        case 3:
+            //            Console.WriteLine(" *** ");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("*****");
+            //            break;
+            //    }
+            //}
+            Console.Write("  ");
+            Console.WriteLine("*");
+            Console.Write(" ");
+            Console.WriteLine("***");
+            Console.WriteLine("*****");
+            Console.Write(" ");
+            Console.WriteLine("***");
+            Console.Write("  ");
+            Console.WriteLine("*");
+
+        }
+    }
+}
+
