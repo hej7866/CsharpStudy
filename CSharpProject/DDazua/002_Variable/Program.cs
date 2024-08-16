2af7a0a043ad0d62df48986c3fd3f28093e16082
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-17 00:11:38 +0900

    2024-08-16 C# 1-4 Study

---
+++ CSharpProject/DDazua/002_Variable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 변수 사용법
+ * 다음 예제를 참고하세요.
+ */
+
+namespace _002_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언에 기초 방법
+            {
+                int number;
+                number = 10;
+                Console.WriteLine(number);
+            }
+
+            // 변수 선언의 다양한 방법
+            {
+                int x;
+                int a, b, c;
+
+                int xx = 100;
+                int aa = 0, bb = 1, cc = 2;
+            }
+        }
+    }
+}
