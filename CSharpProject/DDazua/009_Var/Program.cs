dd82847e70ee94ba49ea23a42cacc3cfaaa3fa44
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-30 23:44:58 +0900

    2024-08-30 C# 1-8 Study (var, nullable)

---
+++ CSharpProject/DDazua/009_Var/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace _009_Var
+{
+    internal class Program
+    {
+        // var numVar = 100;              에러 : 불가(멤버 변수 사용불가)
+
+        // static var Func() {}           에러 : 함수의 반환형으로 사용 불가
+        // static var Func(var a, var b)  에러 : 함수의 파라미터로 사용 불가
+        static void Main(string[] args)
+        {
+            var numVar = 100;
+            var numFVar = 0.0043f;
+
+            Console.WriteLine($"numVar: {numVar}");
+            Console.WriteLine($"numFVar: {numFVar}");
+
+            // var numNull = null;  null초기값은 사용 불가
+        }
+    }
+}
---
+++ CSharpProject/DDazua/010_Nullable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace _010_Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = 100;
+            double? dnum = 3312312453123.323d;
+
+            Console.WriteLine($"num: {num}");
+            Console.WriteLine($"num: {num.Value}");
+            Console.WriteLine($"num: {num.HasValue}");
+            Console.WriteLine($"Type: {num.GetType()}");
+
+            Console.WriteLine($"dnum: {dnum}");
+            Console.WriteLine($"dnum: {dnum.Value}");
+            Console.WriteLine($"dnum: {dnum.HasValue}");
+            Console.WriteLine($"Type: {dnum.GetType()}");
+
+            num = null;
+            Console.WriteLine($"num: {num.GetValueOrDefault()}");
+            Console.WriteLine($"num: {num.HasValue}");
+            
+
+            {
+                int? nullNum = 100;
+                int numCast = (int)nullNum; // 형변환을 해줘야 넣어줄수있다.
+            }
+        }
+    }
+}
